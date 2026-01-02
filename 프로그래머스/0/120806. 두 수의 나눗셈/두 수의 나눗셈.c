b6dabd7e6f91d6f5f5777cce5297506e7ea9b670
Tamoon <81087538+sksqlrlek@users.noreply.github.com>
2026-01-03 08:20:18 +0900

    Delete 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.c

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int num1, int num2) {
-    double value = ((double)num1 / num2) * 1000;
-    int answer = (int) value;
-    return answer;
-}

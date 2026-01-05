9a19de9bc3685847d2b1bca8d1a26027af85fc05
Tamoon <81087538+sksqlrlek@users.noreply.github.com>
2026-01-05 12:04:40 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 94.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        double sum = 0;
+        for(int num : numbers) {
+            sum += num;
+        }
+        answer = sum / numbers.length;
+        return answer;
+    }
+}

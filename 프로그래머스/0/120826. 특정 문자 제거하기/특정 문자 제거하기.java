3b51d7641ec2f9e3c99245a417fe1a5c3e12b66d
Tamoon <81087538+sksqlrlek@users.noreply.github.com>
2026-01-08 08:35:11 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 11.08 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        for(int i = 0; i < my_string.length(); i++) {
+            if(!(String.valueOf(my_string.charAt(i)).equals(letter))) {
+                answer += my_string.charAt(i);
+            }
+                
+        }
+        return answer;
+    }
+}

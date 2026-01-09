7b6b5d6fedf35cfd5d02cb0c2b2668e31a7c2e29
Tamoon <81087538+sksqlrlek@users.noreply.github.com>
2026-01-09 10:46:46 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        for(int i = 0; i < answer.length; i++){
+            int rank = 1;
+            for(int j = 0; j < emergency.length; j++) {
+                if(emergency[j] > emergency[i]) {
+                    rank++;
+                }
+            }
+            answer[i] = rank;
+
+        }
+        return answer;
+    }
+}

02a0170ac76a280f7fcf67ad2168d4e2b9382091
Tamoon <81087538+sksqlrlek@users.noreply.github.com>
2026-01-19 08:51:24 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.14 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] list = s.split(" ");
+        for (int i = 0; i < list.length; i++) {
+            if(list[i].equals("Z")) {
+                answer -= Integer.parseInt(list[i - 1]);
+                continue;
+            }
+            answer += Integer.parseInt(list[i]);
+        }
+        return answer;
+    }
+}
